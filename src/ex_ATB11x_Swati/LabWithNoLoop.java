7b67d477627d19f685f844833b08975047ef6f28
SKADAM21 <swati.vkadam@gmail.com>
2025-03-11 14:33:23 +0530

    No Loop program Done

---
+++ src/ex_ATB11x_Swati/LabWithNoLoop.java
@@ -0,0 +1,21 @@
+package ex_ATB11x_Swati;
+
+public class LabWithNoLoop {
+    public static void main(String[] args)
+    {
+        int table = 9;
+
+        System.out.printf("%d x 1 = %d\n", table, table);
+        System.out.printf("%d x 2 = %d\n", table, table * 2);
+        System.out.printf("%d x 3 = %d\n", table, table * 3);
+        System.out.printf("%d x 4 = %d\n", table, table * 4);
+        System.out.printf("%d x 5 = %d\n", table, table * 5);
+        System.out.printf("%d x 6 = %d\n", table, table * 6);
+        System.out.printf("%d x 7 = %d\n", table, table * 7);
+        System.out.printf("%d x 8 = %d\n", table, table * 8);
+        System.out.printf("%d x 9 = %d\n", table, table * 9);
+        System.out.printf("%d x 10 = %d\n", table, table * 10);
+    }
+}
+
+
